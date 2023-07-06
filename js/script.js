9b6f29e8482f2516994ced975e848a665db6c2ce
EugeniuPeruzzi <jaka1997@live.it>
2023-07-06 16:45:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    },
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+
+}).mount('#app')
